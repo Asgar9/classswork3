cf79b1bf49fc94f24e814a827872354baaa47431
Asgar Y. Asgarov <asgaryas@code.local>
2022-08-05 20:15:18 +0400

    tap arraydaki kenardaki eded uygun indexi

---
+++ finder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            int[] com = { 3, 7, 5, 9 };
+            Console.WriteLine(finder(num,com));
+        }
+        static int finder(int num, int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]==num)
+                {
+                    return i;
+                }
+              
+            }
+             return -1;
+        } 
+    }
+}
